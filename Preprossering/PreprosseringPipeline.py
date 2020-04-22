3023150f30cdaf08084db2ed6444f944b130f818
s175548 <57710093+s175548@users.noreply.github.com>
2020-04-22 13:25:00 +0200

    hej

--- Preprossering/PreprosseringPipeline.py
+++ Preprossering/PreprosseringPipeline.py
@@ -21,7 +21,8 @@ import re
 
 
 class preprossingPipeline:
-    def __init__(self,mac=False,BC_datapath=r"/Users/villadsstokbro/Dokumenter/DTU/KID/3. semester/Fagprojekt/BrainCapture/dataEEG"):
+    def __init__(self,mac=False,BC_datapath=r"/Users/villadsstokbro/Dokumenter/DTU/KID/3. "
+                                            r"semester/Fagprojekt/BrainCapture/dataEEG"):
         """
         args BC datapath: your local path to bc dataset.
         """
@@ -122,6 +123,45 @@ class preprossingPipeline:
             windowOut = None
         return windowOut
 
+    def make_label(self,make_spectograms=True, quality=None, is_usable =None, max_files=10, path = '/Users/villadsstokbro/Dokumenter/DTU/KID/3. semester/Fagprojekt/spectograms_all_ch/',seed=0):
+        i=0
+        if quality is not None:
+            label_dict = {key:int(self.edfDict[key]["annotation"]['Quality Of Eeg']) for key in self.edfDict.keys()}
+            fileNames = [key for key in self.edfDict.keys() if np.any(label_dict[key] == np.array(quality))]
+        elif is_usable is not None:
+            usable_category={'No':0,'Yes':1}
+            label_dict = {key: usable_category[is_usable] for key in
+                            self.edfDict.keys() if self.edfDict[key]["annotation"]["Is Eeg Usable For Clinical Purposes"] == is_usable }
+            fileNames = list(label_dict.keys())
+        else:
+            label_dict={key:idx for idx, key in enumerate(self.edfDict.keys())}
+            fileNames = list(self.edfDict.keys())
+        np.random.seed(seed)
+        np.random.shuffle(fileNames)
+        for filename in fileNames:
+            if i==max_files:
+                break
+            if not os.path.exists(path+filename + '.npy'):
+                pass
+            else:
+                if i == 0:
+                    spectogram = np.load(path+filename + '.npy')
+                    if make_spectograms:
+                        spectogram=spectogram.squeeze()
+                    spectograms = spectogram
+                    labels = np.ones(spectogram.shape[0])*label_dict[filename]
+                else:
+                    spectogram = np.load(path + filename + '.npy')
+                    if make_spectograms:
+                        spectogram=spectogram.squeeze()
+                    spectograms = np.vstack((spectograms, spectogram))
+                    print(label_dict[filename])
+                    label=np.ones(spectogram.shape[0]) * label_dict[filename]
+                    labels=np.concatenate((labels,label))
+            i+=1
+        return spectograms, labels
+
+
 def plot_spectrogram(windows,win_idx,ch_idx):
     win_name=windows.keys()[win_idx]
     ch_name=windows[win_name].keys()
@@ -160,9 +200,5 @@ def getFeatureVec(windowValues,model):
             featureVec = torch.cat((featureVec, tempFeatureVec), 1)
     return featureVec
 
-if __name__=="__main__":
-    C=preprossingPipeline(mac=True)
-    windows=C.get_spectrogram("sbs2data_2018_09_01_08_04_51_328.edf")
-    win_idx=windows.keys()
-    ch_idx=win_idx
-
+C=preprossingPipeline(mac=True)
+a,b= C.make_label(max_files=30,quality=None,is_usable=None,make_spectograms=False,path ='/Users/villadsstokbro/Dokumenter/DTU/KID/3. semester/Fagprojekt/feature_vectors/')
--- Villads/Generate_feature_vectors.py
+++ Villads/Generate_feature_vectors.py
@@ -8,7 +8,6 @@ import os
 model = VGG16_NoSoftmax_OneChannel()
 model.eval()
 C=preprossingPipeline(BC_datapath=r"/Users/villadsstokbro/Dokumenter/DTU/KID/3. semester/Fagprojekt/BrainCapture/dataEEG",mac=True)
-fileNames = C.edfDict.keys()
 wdir="/Users/villadsstokbro/Dokumenter/DTU/KID/3. semester/Fagprojekt"
 for file in fileNames:
     if os.path.exists(wdir+r'/feature_vectors/'+file+".npy")==True:
