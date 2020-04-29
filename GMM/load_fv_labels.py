b8e4f6932d99b92c99e889fe612e62c7bbf065b7
s175549 <s175549@student.dtu.dk>
2020-04-29 10:04:34 +0200

    load_fv_labels

--- GMM/load_fv_labels.py
+++ GMM/load_fv_labels.py
@@ -9,6 +9,6 @@ C = preprossingPipeline(
     BC_datapath=r"C:\Users\johan\iCloudDrive\DTU\KID\4. semester\Fagprojekt\Data\dataEEG")
 path=r'C:\Users\johan\iCloudDrive\DTU\KID\4. semester\Fagprojekt\feature_vectors'
 
-feature_vectors_1,labels_1,filenames= C.make_label(make_spectograms=False,quality=[1],is_usable=None,max_files=5,path = path)
-feature_vectors_9_10,labels_9_10= C.make_label(make_spectograms=False,quality=[9,10],is_usable=None,max_files=5,path = path)
+feature_vectors_1,labels_1,filenames_1= C.make_label(make_spectograms=False,quality=[1],is_usable=None,max_files=5,path = path)
+feature_vectors_9_10,labels_9_10,filenames_9_10= C.make_label(make_spectograms=False,quality=[9,10],is_usable=None,max_files=5,path = path)
 feature_vectors=np.vstack(feature_vectors_1,feature_vectors_9_10)
