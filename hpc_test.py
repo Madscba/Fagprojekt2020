4496ec4e995abb8fe389d1e760aa9938050c8ad6
Mads C. B. Andersen <s173934@student.dtu.dk>
2020-06-05 14:11:46 +0200

    update hpc

--- hpc_test.py
+++ hpc_test.py
@@ -1,8 +1,10 @@
 import numpy as np
-
-print("Hello World!")
+import subprocess
 
 l1 = np.append(np.repeat('Yes',40),np.repeat('No',40))
-with open("test.txt", "w") as myfile:
+
+
+with open("test.txt", "w+") as myfile:
     for ele in l1:
         myfile.write(ele+'\n')
+
