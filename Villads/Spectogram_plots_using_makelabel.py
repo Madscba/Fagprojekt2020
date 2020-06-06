20ec552b637b33f2f0c37f93c5d299f0eed6760b
s175548 <57710093+s175548@users.noreply.github.com>
2020-06-06 15:28:46 +0200

    ...

--- Villads/Spectogram_plots_using_makelabel.py
+++ Villads/Spectogram_plots_using_makelabel.py
@@ -1,5 +1,6 @@
-from Villads.Load_PCA_TSNE import pca
-from Preprossering.PreprosseringPipeline import preprossingPipeline,make_pca
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from Preprossering.PreprosseringPipeline import preprossingPipeline
 from Villads.PCA_TSNE_classes import scale_data
 from Visualization.PCA_TSNE_plot import plot_pca
 import numpy as np
@@ -7,23 +8,22 @@ import numpy as np
 path='/Users/villadsstokbro/Dokumenter/DTU/KID/3. semester/Fagprojekt/spectograms_all_ch/'
 C=preprossingPipeline(BC_datapath='/Users/villadsstokbro/Dokumenter/DTU/KID/3. semester/Fagprojekt/BrainCapture/dataEEG',mac=True)
 
-feature_vectors_1,labels_1,filenames,_= C.make_label(max_files=5,quality=[1],is_usable=None,make_spectograms=True,path = path,seed=10)
-feature_vectors_9_10,labels_9_10,filenames1,_= C.make_label(max_files=5,quality=[9,10],is_usable=None,make_spectograms=True,path = path,seed=10)
+feature_vectors_1,labels_1,filenames,idx= C.make_label(max_files=5,quality=[1],is_usable=None,make_spectograms=True,path = path)
+feature_vectors_9_10,labels_9_10,filenames_1,idx_1= C.make_label(max_files=5,quality=[9,10],is_usable=None,make_spectograms=True,path = path)
 
 feature_vectors=np.vstack((feature_vectors_1,feature_vectors_9_10))
-filenames=filenames+filenames1
+filenames=filenames+filenames_1
 labels=labels_1+labels_9_10
-pca_vectors=make_pca(feature_vectors,make_spectograms=True)
-plot_pca(pca_vectors,labels,np.unique(labels))
-
-
-feature_vectors,labels, _,_= C.make_label(max_files=100, make_from_filenames=filenames, path = path, make_spectograms=True)
 scaled_feature_vectors=scale_data(feature_vectors)
+pca=PCA()
+tsne=TSNE()
+pca.fit(scaled_feature_vectors)
 pca_vectors=pca.transform(scaled_feature_vectors)
+tsne_vectors=tsne.fit_transform(scaled_feature_vectors)
+labels_new=[a[0] for a in idx+idx_1]
+
 plot_pca(pca_vectors,labels,np.unique(labels))
-
-
-
-
-
+plot_pca(tsne_vectors,labels,np.unique(labels),model='TSNE')
+plot_pca(pca_vectors,labels_new,np.unique(labels_new))
+plot_pca(tsne_vectors,labels_new,np.unique(labels_new),model='TSNE')
 
