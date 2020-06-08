453c5f3d1f3ad4e7230c2af9f5f41307dc3e5010
Mads C. B. Andersen <s173934@student.dtu.dk>
2020-06-08 11:29:58 +0200

    hpc

---
+++ hpc_script1.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#BSUB -J HPC_script
+#BSUB -o HPC_script_%J.out
+#BSUB -q hpc
+#BSUB -n 1
+#BSUB -R "rusage[mem=2G]"
+#BSUB -R "span[hosts=1]"
+#BSUB -W 5:00
+# end of BSUB options
+
+echo "Running script..."
+cd ..
+source classifier-env/bin/activate
+cd Fagprojekt2020/Classifier_experimentOne_isUsable
+python3 classifier_experiment_isUsable.py
