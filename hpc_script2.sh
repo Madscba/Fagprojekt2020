ad46ef83282e327b8976f70d6bc9016d6f32576b
Mads C. B. Andersen <s173934@student.dtu.dk>
2020-06-09 17:51:52 +0200

    hpc2 script

--- hpc_script2.sh
+++ hpc_script2.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 #BSUB -J hpc_script2
 #BSUB -o hpc_script2_%J.out
-#BSUB -q hpc
+#BSUB -q gpuv100
 #BSUB -n 1
-#BSUB -R "rusage[mem=16G]"
+#BSUB -R "rusage[mem=64G]"
 #BSUB -R "span[hosts=1]"
-#BSUB -W 5:00
+#BSUB -W 15:00
+#BSUB -u s173934@win.dtu.dk
+#BSUB -N
 # end of BSUB options
 
 echo "Running script..."
 source classifier-env/bin/activate
-python3 Classifier_experimentOne_isUsable/classifier_experiment_isUsable.py
+python3 Classifier_experimentOne_isUsable/Classifier_test.py
