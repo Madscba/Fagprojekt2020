ced356c37c36436a1b4e20654bb7cb997555ce2a
Mads C. B. Andersen <s173934@student.dtu.dk>
2020-06-09 07:33:58 +0200

    hpc script update

--- hpc_script1.sh
+++ hpc_script1.sh
@@ -2,10 +2,10 @@
 #BSUB -J hpc_script2
 #BSUB -o hpc_script2_%J.out
 #BSUB -q hpc
-#BSUB -n 1
+#BSUB -n 4
 #BSUB -R "rusage[mem=64G]"
 #BSUB -R "span[hosts=1]"
-#BSUB -W 10:00
+#BSUB -W 48:00
 # end of BSUB options
 
 echo "Running script..."
