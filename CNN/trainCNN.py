3b1fa7cd24804c4a1f89542c53398524cf0fd192
Mads C. B. Andersen <s173934@student.dtu.dk>
2020-06-10 22:49:49 +0200

    gpuv80 hpc script

--- CNN/trainCNN.py
+++ CNN/trainCNN.py
@@ -133,13 +133,13 @@ def split_dataset(C,path,N,train_split,max_windows,num_channels):
     l2 = labels1[n1:]+labels2[n2:]
 
     return w1, w2, l1, l2
-
+print("Made it so far _136")
 C = preprossingPipeline(BC_datapath=r"/work3/s173934/Fagprojekt/dataEEG")
 path_s = r'/work3/s173934/Fagprojekt/spectograms_rgb'
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.005)
-
+print("Made it so far _142")
 X_train, X_valid, Y_train, Y_valid = split_dataset(C,path_s,N=50,train_split=80,max_windows=10,num_channels=7)
 
 train_acc, train_loss, val_acc, val_loss, model = test_CNN(model,X_train,Y_train,X_valid,Y_valid,batch_size=10,num_epochs=2,preprocessed=True)
