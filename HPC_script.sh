3df6dab64cdd2336747ddbed9bcf11570022df55
Mads C. B. Andersen <s173934@student.dtu.dk>
2020-06-10 17:31:51 +0200

    CNN test hpc script

--- HPC_script.sh
+++ HPC_script.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-#BSUB -J HPC_script
-#BSUB -o HPC_script_%J.out
-#BSUB -q gpuk80
-#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -J feature_script1
+#BSUB -o feature_script1_%J.out
+#BSUB -q hpc
 #BSUB -n 1
-#BSUB -R "rusage[mem=2G]"
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -R "rusage[mem=14G]"
 #BSUB -R "span[hosts=1]"
-#BSUB -W 5:00
+#BSUB -W 10:00
+#BSUB -u s173934@win.dtu.dk
+#BSUB -N
 # end of BSUB options
 
 echo "Running script..."
-cd ..
 source classifier-env/bin/activate
-cd Fagprojekt2020/Classifier_experimentOne_isUsable
-python3 classifier_experiment_isUsable.py
+python3 CNN/trainCNN.py
