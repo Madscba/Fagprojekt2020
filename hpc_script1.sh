88e4c123eb1377dc434a3380fe32167598bf53b2
Mads C. B. Andersen <s173934@student.dtu.dk>
2020-06-10 08:52:01 +0200

    hpc_script to run feature vectors

--- hpc_script1.sh
+++ hpc_script1.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-#BSUB -J full_dataset_script1
-#BSUB -o full_dataset_script1_%J.out
+#BSUB -J feature_script1
+#BSUB -o feature_script1_%J.out
 #BSUB -q gpuv100
 #BSUB -n 1
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -R "rusage[mem=64G]"
 #BSUB -R "span[hosts=1]"
-#BSUB -W 48:00
+#BSUB -W 24:00
 #BSUB -u s173934@win.dtu.dk
 #BSUB -N
 # end of BSUB options
