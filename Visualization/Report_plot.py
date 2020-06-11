c90c317ded774c8c7d3dcd6f6195b298f4172e45
AndreasRaaskov <Andreas@raaskov.dk>
2020-06-11 09:21:53 +0200

    Added some comments

--- Visualization/Report_plot.py
+++ Visualization/Report_plot.py
@@ -21,35 +21,35 @@ feature_vectors_1,labels_1,filenames,idx= C.make_label(max_files=5,quality=[1],i
 feature_vectors_9_10,labels_9_10,filenames_1,idx_1= C.make_label(max_files=5,quality=[9],is_usable=None,path = path, max_windows=40)
 feature_vectors_5,labels_5,filenames_2,idx_2= C.make_label(max_files=5,quality=[5],is_usable=None,path = path, max_windows=40)
 
-# sp1, l1, f1, i1 = C.make_label(max_files=5,quality=[1],is_usable=None,path = path2, max_windows=40)
-# sp2, l2, f2, i2 = C.make_label(max_files=5,quality=[5],is_usable=None,path = path2, max_windows=40)
-# sp3, l3, f3, i3 = C.make_label(max_files=5,quality=[9],is_usable=None,path = path2, max_windows=40)
-#
-# sp = np.vstack((sp1, sp2))
-# sp = np.vstack((sp,sp3))
-# f = f1+f2+f3
-# l = l1+l2+l3
-# i = i1+i2+i3
-# ln =[a[0] for a in i1+i2+i3]
-# ssp = scale_data(sp)
-# pca2=PCA()
-# tsne2=TSNE()
-# clf2 = LinearDiscriminantAnalysis()
-# pca2.fit(ssp)
-# pca_v = pca2.transform(ssp)
-#
-# tsne_v=tsne2.fit_transform(ssp)
-# clf2.fit(ssp,l)
-# lda_v = clf2.transform(ssp)
-# le2 = preprocessing.LabelEncoder()
-# le2.fit(ln)
-#
-# plot_pca_interactiv(pca_v,l,i,model='PCA spectograms')
-# plot_pca_interactiv(tsne_v,l,i,model='TSNE spectograms')
-# plot_pca_interactiv(lda_v,l,i,model='LDA spectograms')
-# plot_pca_interactiv(pca_v,ln,i,model='PCA spectograms')
-# plot_pca_interactiv(tsne_v,ln,i,model='TSNE spectograms')
-# plot_pca_interactiv(lda_v,ln,i,model='LDA spectograms')
+sp1, l1, f1, i1 = C.make_label(max_files=5,quality=[1],is_usable=None,path = path2, max_windows=40)
+sp2, l2, f2, i2 = C.make_label(max_files=5,quality=[5],is_usable=None,path = path2, max_windows=40)
+sp3, l3, f3, i3 = C.make_label(max_files=5,quality=[9],is_usable=None,path = path2, max_windows=40)
+
+sp = np.vstack((sp1, sp2))
+sp = np.vstack((sp,sp3))
+f = f1+f2+f3
+l = l1+l2+l3
+i = i1+i2+i3
+ln =[a[0] for a in i1+i2+i3]
+ssp = scale_data(sp)
+pca2=PCA()
+tsne2=TSNE()
+clf2 = LinearDiscriminantAnalysis()
+pca2.fit(ssp)
+pca_v = pca2.transform(ssp)
+
+tsne_v=tsne2.fit_transform(ssp)
+clf2.fit(ssp,l)
+lda_v = clf2.transform(ssp)
+le2 = preprocessing.LabelEncoder()
+le2.fit(ln)
+
+plot_pca_interactiv(pca_v,l,i,model='PCA spectograms')
+plot_pca_interactiv(tsne_v,l,i,model='TSNE spectograms')
+plot_pca_interactiv(lda_v,l,i,model='LDA spectograms')
+plot_pca_interactiv(pca_v,ln,i,model='PCA spectograms')
+plot_pca_interactiv(tsne_v,ln,i,model='TSNE spectograms')
+plot_pca_interactiv(lda_v,ln,i,model='LDA spectograms')
 
 
 fv=np.vstack((feature_vectors_1,feature_vectors_5))
