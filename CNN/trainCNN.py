c44ddbf4333558b2ae6d3767db0da458b511ad66
Mads C. B. Andersen <s173934@student.dtu.dk>
2020-06-11 07:49:33 +0200

    classifier_test ready to run features on hpc

--- CNN/trainCNN.py
+++ CNN/trainCNN.py
@@ -135,11 +135,11 @@ def split_dataset(C,path,N,train_split,max_windows,num_channels):
     return w1, w2, l1, l2
 
 C = preprossingPipeline(BC_datapath=r"/work3/s173934/Fagprojekt/dataEEG")
-from OSS import test
 path_s = r'/work3/s173934/Fagprojekt/spectograms_rgb'
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.005)
+from OSS import test
 X_train, X_valid, Y_train, Y_valid = split_dataset(C,path_s,N=50,train_split=80,max_windows=10,num_channels=7)
 train_acc, train_loss, val_acc, val_loss, model = test_CNN(model,X_train,Y_train,X_valid,Y_valid,batch_size=10,num_epochs=2,preprocessed=True)
 #print("\n Final training accuracy: ", train_acc)
