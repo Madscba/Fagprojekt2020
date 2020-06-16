4e642d26fca561ecf480db0f93474f7dffda0f1f
s175549 <s175549@student.dtu.dk>
2020-06-16 21:20:29 +0200

    HPC

--- CNN_HPC/CNN_lr1.py
+++ CNN_HPC/CNN_lr1.py
@@ -68,6 +68,7 @@ def test_CNN(model,X_train,y_train,X_valid,y_valid,w_id,batch_size,num_epochs,pr
         model.eval()
         wrong_guesses = []
         wrong_predictions = []
+        all_predictions = []
         for i in range(num_test_batches):
             if i % 10 == 0:
                 print("\n {}, now validation...".format(i), end='')
@@ -97,6 +98,7 @@ def test_CNN(model,X_train,y_train,X_valid,y_valid,w_id,batch_size,num_epochs,pr
                     wrong_predictions.append(preds[k])
                 else:
                     correct2 += 1
+                all_predictions.append(preds[k])
 
         val_acc = correct2 / float(num_test_samples)
         val_cost.append(np.mean(val_loss))
@@ -105,7 +107,7 @@ def test_CNN(model,X_train,y_train,X_valid,y_valid,w_id,batch_size,num_epochs,pr
             print("\n Epoch %2i : Train Loss %f , Train acc %f, Valid acc %f" % (
                 epoch + 1, train_cost[-1], train_acc, val_acc))
 
-    return train_acc,train_cost,val_acc,val_cost, wrong_guesses, wrong_predictions, model
+    return train_acc,train_cost,val_acc,val_cost, wrong_guesses, wrong_predictions, all_predictions, model
 
 def split_dataset(C,path,N,train_split,max_windows,num_channels):
     """ Input: Data and training split (in %)
@@ -114,27 +116,36 @@ def split_dataset(C,path,N,train_split,max_windows,num_channels):
                                                                        is_usable='Yes', max_files=N, max_windows=max_windows,
                                                                        path=path_s, seed=0, ch_to_include=range(num_channels))
     windows2, labels2, filenames2, window_idx_full2 = C.make_label_cnn(make_from_filenames=None, quality=None,
-                                                                       is_usable='No', max_files=N, max_windows=np.maximum(int(round(len(filenames1)/26*max_windows)),1),
+                                                                       is_usable='No', max_files=N, max_windows=max_windows,
                                                                        path=path_s, seed=0, ch_to_include=range(num_channels))
-    n_train_files1 = int(len(filenames1) / 10 * (train_split/10))
+    key = np.maximum(int(round(len(filenames1) / len(filenames2))),1)
+    windows3, labels3, filenames3, window_idx_full3 = C.make_label_cnn(make_from_filenames=None, quality=None,
+                                                                       is_usable='Yes', max_files=N, max_windows=int(round(max_windows/key)),
+                                                                       path=path_s, seed=0, ch_to_include=range(num_channels))
+    n_train_files1 = int(len(filenames3) / 10 * (train_split/10))
     n_train_files2 = int(len(filenames2) / 10 * (train_split/10))
     a = np.array(window_idx_full1)
     b = np.array(window_idx_full2)
+    c = np.array(window_idx_full3)
     j1 = np.unique(a[:, 0], return_counts=True)
     j2 = np.unique(b[:,0], return_counts=True)
+    j3 = np.unique(c[:, 0], return_counts=True)
     j1 = np.array(j1)
     j2 = np.array(j2)
+    j3 = np.array(j3)
     j1 = np.asarray(j1[1, :], dtype='int64')
     j2 = np.asarray(j2[1,:], dtype='int64')
+    j3 = np.asarray(j3[1, :], dtype='int64')
     n1 = np.sum(j1[:n_train_files1])
     n2 = np.sum(j2[:n_train_files2])
-    wt1 = windows1[:n1,:,:,:]
+    n3 = np.sum(j3[:n_train_files1])
+    wt1 = windows3[:n3,:,:,:]
     wt2 = windows2[:n2,:,:,:]
     w1 = torch.cat((wt1,wt2))
     ww1 = windows1[n1:,:,:,:]
     ww2 = windows2[n2:,:,:,:]
     w2 = torch.cat((ww1,ww2))
-    l1 = labels1[:n1]+labels2[:n2]
+    l1 = labels3[:n3]+labels2[:n2]
     l2 = labels1[n1:]+labels2[n2:]
     wid = window_idx_full1[n1:]+window_idx_full2[n2:]
     train_windows, train_labels = shuffle(w1,l1)
@@ -153,21 +164,21 @@ freeze_parameters(modelB,feature_extracting=True)
 for i in range(2):
     PATH = '/zhome/87/9/127623/Fagprojekt'
     if i == 0:
-        activation_list = np.array([26, 27, 28, 29, 30, 31])
+        activation_list = np.array([28, 29, 30, 31])
         grad_parameters(modelA, list(list2[activation_list]))
         optimizer = optim.Adam(modelA.parameters(), lr=0.001)
-        train_acc, train_loss, val_acc, val_loss, wrong_guesses, wrong_predictions, modelA = test_CNN(modelA, X_train, Y_train, X_valid,
-                                                                                  Y_valid, windows_id, batch_size=128,
-                                                                                  num_epochs=4, preprocessed=True)
+        train_acc, train_loss, val_acc, val_loss, wrong_guesses, wrong_predictions, all_pred, modelA = test_CNN(modelA, X_train, Y_train, X_valid,
+                                                                                  Y_valid, windows_id, batch_size=256,
+                                                                                  num_epochs=2, preprocessed=True)
         print(("Finished run nr. %i, modelA is now saved:")%i)
         torch.save(modelA.state_dict(), 'model0_l1.pt')
     else:
-        activation_list = np.array([20,21,22,23,24,25,26, 27, 28, 29, 30, 31])
+        activation_list = np.array([24, 25, 26, 27, 28, 29, 30, 31])
         grad_parameters(modelB, list(list2[activation_list]))
         optimizer = optim.Adam(modelB.parameters(), lr=0.001)
-        train_acc, train_loss, val_acc, val_loss, wrong_guesses, wrong_predictions, modelB = test_CNN(modelB, X_train, Y_train, X_valid,
-                                                                                  Y_valid, windows_id, batch_size=128,
-                                                                                  num_epochs=4, preprocessed=True)
+        train_acc, train_loss, val_acc, val_loss, wrong_guesses, wrong_predictions, all_pred, modelB = test_CNN(modelB, X_train, Y_train, X_valid,
+                                                                                  Y_valid, windows_id, batch_size=256,
+                                                                                  num_epochs=2, preprocessed=True)
         torch.save(modelB.state_dict(), 'model1_l1.pt')
 
     train_acc_data = np.asarray(train_acc)
@@ -183,6 +194,8 @@ for i in range(2):
     np.save((f'wrong_guesses_l1_{i}.npy'), wrong_guesses_data)
     wrong_pred_data = np.asarray(wrong_predictions)
     np.save((f'wrong_guesses_class_l1_{i}.npy'), wrong_pred_data)
+    all_pred_data = np.asarray(all_pred)
+    np.save((f'all_guesses_l1_{i}.npy'), all_pred_data)
 
 torch.save(X_valid,'test_set.pt')
 np.save('test_labels.npy',np.asarray(Y_valid))
