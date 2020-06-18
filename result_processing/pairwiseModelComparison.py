9f506ea40c51704536ab33a603d69ccfd724eaff
Mads C. B. Andersen <s173934@student.dtu.dk>
2020-06-18 15:05:37 +0200

    pairwise model comp.

--- result_processing/pairwiseModelComparison.py
+++ result_processing/pairwiseModelComparison.py
@@ -2,6 +2,7 @@ import scipy.stats
 import numpy as np
 import scipy.stats as st
 import json, os
+import pandas as pd
 
 #FUNCTION COPIED FROM 02450 COURSE MATERIALS- AUTHOR TUE HERLAU
 def jeffrey_interval(y, yhat, alpha=0.05):
@@ -52,7 +53,7 @@ def mcnemar(y_true, yhatA, yhatB, alpha=0.05):
     return thetahat, CI, p
 
 #Get labels y_true and predictions y_hat
-def extractLabelsAndModelPredictions(path,filename,classifiers):
+def extractLabelsAndModelPredictionsJson(path, filename, classifiers):
     y_true,y_hat = np.array([]),np.array([])
     filepath = os.path.join(path, filename)
     with open(filepath) as json_file:
@@ -69,31 +70,43 @@ def extractLabelsAndModelPredictions(path,filename,classifiers):
 
         # ex_balanced_train_fea0
         # ex_unbalanced_train_spec0
-def extractInformationFromFile():
+def extractInformationTxtFile():
     pass
     return
 
 def computeJeffreyIntervals(path = r"C:\Users\Mads-\OneDrive\Dokumenter\Universitet\4. Semester\downloads from hpc\ClassifierTestLogs. Ex_10_split_bal_unbal",classifiers=['SVM'],files="",n_splits=10):
     if files == "":
         raise Exception("No files given")
-    n = 10
+    results = np.array([])
     for idx, file in enumerate(files):
-        computeJeffreyInveralFromFile(file,path=path,classifiers=classifiers[idx],n_splits=n_splits)
+        file_results= computeJeffreyInveralFromFile(file,path=path,classifiers=classifiers[idx],n_splits=n_splits)
+        if idx ==0:
+            jeff_interval=file_results
+        else:
+            jeff_interval = pd.concat([jeff_interval,file_results])
+    return jeff_interval
 def computeJeffreyInveralFromFile(file,path="",classifiers=[],n_splits=10):
     alpha = 0.05
+    data = []
     for k in range(n_splits):
         file_path =file.replace('{i}',f'{k}')
-        y_true,y_hat = extractLabelsAndModelPredictions(path, file_path,classifiers=classifiers)
+        y_true,y_hat = extractLabelsAndModelPredictionsJson(path, file_path, classifiers=classifiers)
         for i in range(len(classifiers)):
             [thetahatA, CIA] = jeffrey_interval(y_true, y_hat[0+len(y_true)*i:len(y_true)*(1+i)], alpha=alpha)
-            print(classifiers[i],"|",file_path,"| Theta point estimate", thetahatA, "| CI: ", CIA)
+            data.append([classifiers[i],thetahatA,CIA,file_path])
+    classifier_results = pd.DataFrame(data, columns=['Model', 'ThetaA', 'Confidence interval', 'File'])
+    return classifier_results
 
 
 if __name__ == '__main__':
     files_bal = ['ex_balanced_train_fea{i}_predict.json','ex_balanced_train_spec{i}_predict.json']
     classifiers_bal = [['SVM', 'LDA'],['RF']]
-    computeJeffreyIntervals(classifiers = classifiers_bal,files=files_bal)
-
+    jeff_bal = computeJeffreyIntervals(classifiers = classifiers_bal,files=files_bal)
+    pass
     files_unbal = ['ex_unbalanced_train_fea{i}_predict.json','ex_unbalanced_train_spec{i}_predict.json']
     classifiers_unbal = [['SVM', 'LDA'],['RF']]
-    computeJeffreyIntervals(classifiers=classifiers_unbal, files=files_unbal)
+    jeff_unbal = computeJeffreyIntervals(classifiers=classifiers_unbal, files=files_unbal)
+    pass
+
+    # baseline,SVM, LDA, RF
+    # computeMcNemarComparisons()
